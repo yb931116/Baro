b548acd02bde06b45748c28566753d1da194b756
'' <''>
2018-07-03 17:53:22 +0900

    commit_kt: login 파일 추가

--- src/main/java/com/project/baro/controller/ByPassController.java
+++ src/main/java/com/project/baro/controller/ByPassController.java
@@ -1,9 +1,9 @@
 package com.project.baro.controller;
 
-/*import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-*/
-//@Controller
+
+@Controller
 public class ByPassController {
 	
 	
@@ -11,11 +11,15 @@ public class ByPassController {
 	 * method default�� GET
 	 */
 	
-/*	@RequestMapping(value = "/*")
+	@RequestMapping(value = "/*")
 	public void byPass() {	
 		
-	}*/
+	}
 	
+	@RequestMapping(value = "/*/*")
+	public void byPass2() {	
+		
+	}
 
 	
 
