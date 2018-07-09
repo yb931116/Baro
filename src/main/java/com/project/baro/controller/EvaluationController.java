142944d9979caf8079c6c74d73462dcbb41d4ea3
heegeon <gmlrjs9503@gmail.com>
2018-07-09 11:30:09 +0900

    evaluation 처리

---
+++ src/main/java/com/project/baro/controller/EvaluationController.java
@@ -0,0 +1,37 @@
+package com.project.baro.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EvaluationController {
+	private final static String MAPPING = "/evaluation";
+	
+	@RequestMapping(value = MAPPING+"{action}", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap ,@PathVariable String action,
+			ModelAndView modelandView) {
+		
+		String viewName = MAPPING + action;
+		Map<String,Object> resultMap = new HashMap<String, Object>();
+		List<Object> resultList = new ArrayList<Object>();
+		
+		if("".equalsIgnoreCase(action)){
+			//TODO 서비스 연동 필요
+		}
+		
+		modelandView.setViewName(viewName);
+		modelandView.addObject("paramMap",paramMap);
+		modelandView.addObject("resultMap",resultMap);
+		modelandView.addObject("resultList",resultList);
+		return modelandView;
+	}
+}
