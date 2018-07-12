b7c32f6dd3102162de6fef64a529ece43f5f7923
heegeon <gmlrjs9503@gmail.com>
2018-07-12 17:09:22 +0900

    평가 완료

--- src/main/java/com/project/baro/controller/EvaluationController.java
+++ src/main/java/com/project/baro/controller/EvaluationController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,6 @@ public class EvaluationController {
 		List<Object> resultList = new ArrayList<Object>();
 		
 		if("".equalsIgnoreCase(action)){
-			//TODO 서비스 연동 필요
 		}
 		
 		modelandView.setViewName(viewName);
