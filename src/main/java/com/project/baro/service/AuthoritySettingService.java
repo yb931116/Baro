916d3911f6e7cb9e1b79126c536d854928257204
'' <''>
2018-07-17 12:38:00 +0900

    kt_commit:작업중

--- src/main/java/com/project/baro/service/AuthoritySettingService.java
+++ src/main/java/com/project/baro/service/AuthoritySettingService.java
@@ -1,4 +1,5 @@
 package com.project.baro.service;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,18 @@ public class AuthoritySettingService {
 			String sqlMapId = "authority_setting.list";
 			Object resultObject = dao.getList(sqlMapId, dataMap);
 
+			return resultObject;
+		}
+		
+		public Object authority_setting(Object dataMap) {
+			Map<String,Object> resultMap = new HashMap<String, Object>();
+			resultMap.put("authority_setting", dataMap );  
+			
+			String sqlMapId = "authority_setting.update";
+			dao.getObject(sqlMapId, resultMap);
+			sqlMapId = "authority_setting.list";
+			Object resultObject = dao.getList(sqlMapId, dataMap);
+			
 			return resultObject;
 		}
 	
