c28990c4e9feecd8ba89202897363f0548d3cf5c
heegeon <gmlrjs9503@gmail.com>
2018-07-19 16:47:04 +0900

    정말 힘들구나 통계 2개햇어

--- src/main/java/com/project/baro/controller/StatisticsController.java
+++ src/main/java/com/project/baro/controller/StatisticsController.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.project.baro.service.StatisticsService;
+
 @Controller
 public class StatisticsController {
 private final static String MAPPING = "/statistics";
-	
+
+	@Autowired
+	StatisticsService service;
+
 	@RequestMapping(value = MAPPING+"{action}", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView actionMethod(@RequestParam Map<String, Object> paramMap ,@PathVariable String action,
 			ModelAndView modelandView) {
@@ -23,11 +29,10 @@ private final static String MAPPING = "/statistics";
 		String viewName = MAPPING + action;
 		Map<String,Object> resultMap = new HashMap<String, Object>();
 		List<Object> resultList = new ArrayList<Object>();
-		
+		System.out.println("왜그렇냐" + action);
 		if("".equalsIgnoreCase(action)){
-			//TODO 서비스 연동 필요
+			 resultList =(List<Object>) service.getListbyID("statistics.getListById",paramMap);
 		}
-		
 		modelandView.setViewName(viewName);
 		modelandView.addObject("paramMap",paramMap);
 		modelandView.addObject("resultMap",resultMap);
---
+++ src/main/java/com/project/baro/service/StatisticsService.java
@@ -0,0 +1,24 @@
+package com.project.baro.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.baro.dao.ShareDao;
+
+@Service
+public class StatisticsService {
+	
+	@Autowired
+	ShareDao dao;
+
+	public Object getListbyID(String SqlMapId, Object dataMap) {
+
+		Object resultList = dao.getList(SqlMapId, dataMap);
+		return resultList;
+	}
+	
+	
+}
